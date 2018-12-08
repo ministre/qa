9917e1f5cfd2676e9240c5600bf4e4f1179f3c53
ministre <vlgamer@mail.ru>
2018-12-09 00:37:32 +0300

    ship it

--- device/urls.py
+++ device/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.type_list, name='type_list'),
+    path('', views.device_list, name='device_list'),
+    path('type/', views.type_list, name='type_list'),
 ]
--- device/views.py
+++ device/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def type_list(request):
     return render(request, 'device/type_list.html')
+
+
+def device_list(request):
+    return render(request, 'device/device_list.html')
