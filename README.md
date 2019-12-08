388a9f62b52120a133e1fe55ef337d814beceb01
ministre <vlgamer@mail.ru>
2019-12-08 16:49:20 +0300

    deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# Quality Assurance
-Experimental version
--- qa/settings/__init__.py
+++ qa/settings/__init__.py
@@ -4,4 +4,3 @@ try:
     from .local import *
 except ImportError:
     print("Can't find module settings/local.py! Make it from settings/local.py.skeleton!")
-
