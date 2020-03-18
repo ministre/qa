7605534c2e0c12ac07e4479da1d9c9fe94a2b907
ministre <vlgamer@mail.ru>
2020-03-18 23:07:17 +0300

    self isolation

--- protocol/views.py
+++ protocol/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Protocol
 from .forms import ProtocolForm
 from django.urls import reverse
+from firmware.models import Firmware
+from testplan.models import Testplan
+from device.models import Device
 
 
 @method_decorator(login_required, name='dispatch')
@@ -23,6 +26,13 @@ class ProtocolCreate(CreateView):
     def get_initial(self):
         return {'device': self.kwargs.get('device_id'),
                 'created_by': self.request.user, 'updated_by': self.request.user}
+
+    def get_form(self, form_class=ProtocolForm):
+        form = super(ProtocolCreate, self).get_form(form_class)
+        device = Device.objects.get(id=self.kwargs.get('device_id'))
+        form.fields['firmware'].queryset = Firmware.objects.filter(device=device)
+        form.fields['testplan'].queryset = Testplan.objects.filter(device_type=device.type)
+        return form
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
