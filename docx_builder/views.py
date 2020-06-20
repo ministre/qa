475e7b3f3701a159d6ad06f71f27d8b0f063cf95
ministre <vlgamer@mail.ru>
2020-06-20 14:19:35 +0300

    self-isolation

--- docx_builder/views.py
+++ docx_builder/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from testplan.models import Testplan
+from docx import Document
+
 
 # Create your views here.
+def build_testplan(request):
+    if request.method == 'POST':
+        testplan = get_object_or_404(Testplan, id=request.POST['testplan'])
+        document = Document()
