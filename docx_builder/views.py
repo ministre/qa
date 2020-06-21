5582f9a8b481c4fda5dfb20004a29fb0da63eb77
ministre <vlgamer@mail.ru>
2020-06-22 00:25:49 +0300

    self-isolation

--- docx_builder/views.py
+++ docx_builder/views.py
@@ -6,13 +6,14 @@ from django.contrib.auth.decorators import login_required
 from .models import DocxProfile
 from .forms import DocxProfileForm
 from django.shortcuts import get_object_or_404
-from testplan.models import Testplan, Category, Test
-from docx import Document
-from docx.shared import Pt, RGBColor
+from testplan.models import Testplan, Chapter, Category, Test, TestLink
 import os
 from django.conf import settings
 from django.http import HttpResponse, Http404
 from datetime import datetime
+from docx import Document
+from docx.shared import Pt, RGBColor
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 
 @method_decorator(login_required, name='dispatch')
@@ -77,14 +78,34 @@ def build_testplan(request):
     if request.method == 'POST':
         testplan = get_object_or_404(Testplan, id=request.POST['testplan'])
         document = Document()
-
+        # styles
+        style = document.styles['Title']
+        style.paragraph_format.space_before = Pt(5)
+        style.font.size = Pt(20)
+        style.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
         style = document.styles['Heading 1']
         style.paragraph_format.space_before = Pt(5)
         style.font.size = Pt(16)
-        style.font.color.rgb = RGBColor(0x77, 0x00, 0xff)
-
+        style.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
+        style = document.styles['Heading 2']
+        style.paragraph_format.space_before = Pt(5)
+        style.font.size = Pt(14)
+        style.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
+        style = document.styles['Subtitle']
+        style.paragraph_format.space_before = Pt(5)
+        style.font.size = Pt(13)
+        style.font.bold = True
+        style.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
+        # title
         document.add_paragraph(testplan.name, style='Title')
-
+        # chapters
+        chapters = Chapter.objects.filter(testplan=testplan).order_by('id')
+        if chapters:
+            for chapter in chapters:
+                document.add_heading(chapter.name, level=1)
+                paragraph = document.add_paragraph(chapter.text, style='Body Text')
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+        # tests
         categories = Category.objects.filter(testplan=testplan).order_by('id')
         for i, category in enumerate(categories):
             document.add_heading(str(i+1) + '. ' + category.name, level=1)
@@ -95,16 +116,23 @@ def build_testplan(request):
 
                 # purpose
                 document.add_paragraph('Цель', style='Subtitle')
-                document.add_paragraph(test.purpose, style='Body Text')
+                paragraph = document.add_paragraph(test.purpose, style='Body Text')
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
                 # procedure
                 document.add_paragraph('Процедура', style='Subtitle')
-                document.add_paragraph(test.procedure, style='Body Text')
+                paragraph = document.add_paragraph(test.procedure, style='Body Text')
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
                 # expected
                 document.add_paragraph('Ожидаемый результат', style='Subtitle')
-                document.add_paragraph(test.expected, style='Body Text')
+                paragraph = document.add_paragraph(test.expected, style='Body Text')
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
                 # links
-                document.add_paragraph('Ссылки', style='Subtitle')
-                document.add_paragraph(test.expected, style='Body Text')
+                links = TestLink.objects.filter(test=test).order_by('id')
+                if links:
+                    document.add_paragraph('Ссылки', style='Subtitle')
+                    for link in links:
+                        document.add_paragraph(link.name, style='Body Text')
+                        document.add_paragraph(link.url, style='List Bullet')
 
         testplan_filename = settings.MEDIA_ROOT + '/testplan_' + str(testplan.id) + '.docx'
         document.save(testplan_filename)
