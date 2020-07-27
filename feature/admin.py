16632cb543c59347d405b610273114cd16622234
ministre <vlgamer@mail.ru>
2020-07-27 13:58:54 +0300

    self-isolation

--- feature/admin.py
+++ feature/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FeatureList, FeatureListCategory, FeatureListItem
+from .models import FeatureList, FeatureListCategory, FeatureListItem, FeatureListLink
 
 admin.site.register(FeatureList)
 admin.site.register(FeatureListCategory)
 admin.site.register(FeatureListItem)
+admin.site.register(FeatureListLink)
