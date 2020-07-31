a40b70c43674d033ed6045dcc82bcbf5efc1213d
ministre <vlgamer@mail.ru>
2020-07-31 18:49:44 +0300

    self isolation

--- testplan/admin.py
+++ testplan/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Testplan, Category, Chapter, Test, TestConfig, TestImage, TestFile, TestChecklist, \
-    TestChecklistItem, TestIntegerValue, TestLink, TestComment, Pattern
+    TestChecklistItem, TestIntegerValue, TestLink, TestComment
 
 admin.site.register(Testplan)
 admin.site.register(Category)
@@ -14,4 +14,3 @@ admin.site.register(TestChecklistItem)
 admin.site.register(TestIntegerValue)
 admin.site.register(TestLink)
 admin.site.register(TestComment)
-admin.site.register(Pattern)
