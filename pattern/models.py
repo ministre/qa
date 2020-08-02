dd2644d0ddf069f494b9a17558e677a3168dcb36
ministre <vlgamer@mail.ru>
2020-08-02 13:46:59 +0300

    self-isolation

--- pattern/models.py
+++ pattern/models.py
@@ -7,9 +7,9 @@ class Pattern(models.Model):
     name = models.CharField(max_length=1000)
     redmine_parent = models.CharField(max_length=1000, blank=True, null=True, default='patterns')
     redmine_project = models.CharField(max_length=1000, blank=True, null=True)
-    created_by = models.ForeignKey(User, related_name='p_c', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='p_c', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(default=datetime.now)
-    updated_by = models.ForeignKey(User, related_name='p_u', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name='p_u', on_delete=models.CASCADE, blank=True, null=True)
     updated_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
@@ -25,9 +25,9 @@ class Pattern(models.Model):
 class PatternCategory(models.Model):
     pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
     name = models.CharField(max_length=1000)
-    created_by = models.ForeignKey(User, related_name='p_c_c', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='p_c_c', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(default=datetime.now)
-    updated_by = models.ForeignKey(User, related_name='p_c_u', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name='p_c_u', on_delete=models.CASCADE, blank=True, null=True)
     updated_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
